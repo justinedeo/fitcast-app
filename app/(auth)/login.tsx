c8212e2babd217f41d9a400e6e818c0c4cdda8e6
justinedeo <justinefdeo@gmail.com>
2026-02-02 18:45:47 -0600

    Made basic login screen & force expo to open 1st

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,2 +1,41 @@
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
+export default function Login() 
+{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => 
+    {
+        console.log("Logging in with:", username, password);
+
+        router.replace("/(tabs)");
+    };
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", padding: 20, backgroundColor: "white"}}>
+            <Text>Login to FitCast</Text>
+
+            <TextInput
+                placeholder = "Username"
+                value={username}
+                onChangeText={setUsername}
+                style={{borderWidth: 1, marginVertical: 10, padding: 8}}
+            />
+
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={{ borderWidth: 1, marginVertical: 10, padding: 8}}
+            />
+
+            <Button title="Sign In" onPress={handleLogin} />
+            </View>
+    );
+}
 
 
