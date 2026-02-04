e577f690a6e30169642c6785b31b1d39a73c4c6f
justinedeo <justinefdeo@gmail.com>
2026-02-04 13:28:43 -0600

    Changed background and added 'Epilogue' font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const [fontsReady] = useFonts({
+    "Epilogue-Medium": require("../assets/fonts/Epilogue-Medium.ttf"),
+    "Epilogue-Bold": require("../assets/fonts/Epilogue-Bold.ttf"),
+    "Epilogue-Regular": require("../assets/fonts/Epilogue-Regular.ttf"),
+    "Epilogue-SemiBold": require("../assets/fonts/Epilogue-SemiBold.ttf"),
+  });
+
+  if (!fontsReady) return null;
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
